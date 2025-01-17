b63a7c3623b00a8d51ef9b1f28f42962c8995708
Jyotiraditya Raghuvanshi <157639841+jyotiradityaraghuvanshi@users.noreply.github.com>
2025-01-17 22:34:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	const form = document.querySelector('#form1');
+	
+	const first = form.querySelector('input[name="fname"]').value;
+	const last = form.querySelector('input[name="lname"]').value;
+
+	alert(first + " " + last);
 
 }
